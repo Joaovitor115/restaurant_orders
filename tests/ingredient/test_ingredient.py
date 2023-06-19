0372ce12c528bd622154ee9a3dbeaac1d0c079af
Joao_Vitor <2013joaovitor10@Gmail.com>
2023-06-19 15:16:01 -0300

    fix: linter

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,17 +1,20 @@
 from src.models.ingredient import Ingredient, Restriction
-from tests.ingredients import INGREDIENTS
+# from tests.ingredients import INGREDIENTS
 
 
 # Req 1
 def test_ingredient():
-    ingredient = Ingredient('queijo provolone')
-    ingredient2 = Ingredient('queijo provolone')
-    ingredient3 = Ingredient('massa de raviole')
+    ingredient = Ingredient("queijo provolone")
+    ingredient2 = Ingredient("queijo provolone")
+    ingredient3 = Ingredient("massa de raviole")
 
     assert ingredient.__hash__() == ingredient.__hash__()
     assert ingredient.__hash__() != ingredient3.__hash__()
     assert ingredient == ingredient2
     assert ingredient != ingredient3
     assert ingredient.__repr__() == "Ingredient('queijo provolone')"
-    assert ingredient.name == 'queijo provolone'
-    assert ingredient.restrictions == {Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
+    assert ingredient.name == "queijo provolone"
+    assert ingredient.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
