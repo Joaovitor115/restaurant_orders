72a39ec9ad9e01f0412f3a4967596aee060087fd
Joao_Vitor <2013joaovitor10@Gmail.com>
2023-06-20 17:35:45 -0300

    teste: codigo requisito 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,27 @@
-# Req 3
+import csv
+from models.dish import Dish
+from models.ingredient import Ingredient
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.source_path = source_path
+
+    # def get_dishes(self):
+        with open(self.source_path, 'r') as file:
+            reader = csv.DictReader(file)
+            csv_content = list(reader)
+            # print(csv_content, 'AAAAAAAAAAAAA')
+            for dish in csv_content:
+                # print(dish, 'CURRENT DISH')
+                current_dish = Dish(dish['dish'], float(dish['price']))
+                self.dishes.add(current_dish)
+                
+                ingredient = Ingredient(dish['ingredient'])
+                current_dish.add_ingredient_dependency(ingredient, int(dish['recipe_amount']))
+            
+            return self.dishes
+
+meu_menu = MenuData('data/menu_base_data.csv')
+meu_menu.get_dishes()
+print(meu_menu.dishes)
