e91f701b156ddbada64720d986e9081558be2d9b
Joao_Vitor <2013joaovitor10@Gmail.com>
2023-06-20 18:10:54 -0300

    requisito 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -2,26 +2,27 @@ import csv
 from models.dish import Dish
 from models.ingredient import Ingredient
 
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
         self.dishes = set()
         self.source_path = source_path
 
-    # def get_dishes(self):
-        with open(self.source_path, 'r') as file:
+        with open(self.source_path, "r") as file:
             reader = csv.DictReader(file)
             csv_content = list(reader)
-            # print(csv_content, 'AAAAAAAAAAAAA')
-            for dish in csv_content:
-                # print(dish, 'CURRENT DISH')
-                current_dish = Dish(dish['dish'], float(dish['price']))
-                self.dishes.add(current_dish)
-                
-                ingredient = Ingredient(dish['ingredient'])
-                current_dish.add_ingredient_dependency(ingredient, int(dish['recipe_amount']))
-            
-            return self.dishes
 
-meu_menu = MenuData('data/menu_base_data.csv')
-meu_menu.get_dishes()
-print(meu_menu.dishes)
+            for dish in csv_content:
+                current_dish = Dish(dish["dish"], float(dish["price"]))
+
+                if current_dish in self.dishes:
+                    current_dish = next(
+                        d for d in self.dishes if d == current_dish
+                    )
+
+                else:
+                    self.dishes.add(current_dish)
+
+                current_dish.add_ingredient_dependency(
+                    Ingredient(dish["ingredient"]), int(dish["recipe_amount"])
+                )
